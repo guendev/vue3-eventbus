d432c8cb9695095ac373336a86d379dc47c00cdd
coder <dnstylish@gmail.com>
2022-06-14 08:52:39 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import mitt from 'mitt'
+import { App } from 'vue'
+const emitter = mitt()
+
+// Cutsom type
+declare module '@vue/runtime-core' {
+    export interface ComponentCustomProperties {
+        $emitter: typeof emitter
+    }
+}
+
+const plugin = {
+    install(app: App) {
+        app.provide('$emitter', emitter)
+        app.config.globalProperties.$emitter = emitter
+    }
+}
+
+export default plugin
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import * as path from 'path'
+import typescript from "@rollup/plugin-typescript"
+
+const resolvePath = (str: string) => path.resolve(__dirname, str)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Vue3EventBus",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+      plugins: [
+        typescript({
+          'target': 'es2020',
+          'rootDir': resolvePath('./src'),
+          'declaration': true,
+          'declarationDir': resolvePath('./dist'),
+          exclude: resolvePath('./node_modules/**'),
+          allowSyntheticDefaultImports: true
+        })
+      ]
+    },
+  },
+})
